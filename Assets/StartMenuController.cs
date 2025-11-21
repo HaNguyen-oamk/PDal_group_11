3081858e162cd14916e7f4a90d9e1131d3a95d7a
hduy0407 <t3ngdu01@students.oamk.fi>
2025-11-21 02:21:13 +0200

    add logic for startmenu

---
+++ Assets/StartMenuController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnStartClick()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    // Update is called once per frame
+    public void OnExitClick()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif 
+            Application.Quit();    
+    }
+}
